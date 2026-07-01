ac7c49b9914a8f258b42cd7d8fd75974979d793d
Lautaro <lautarollames94@gmail.com>
2026-06-30 22:03:34 -0300

    Agrego funcionamiento de cámara QR

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
+window.addEventListener("DOMContentLoaded", () => {
 
+    const qr = new Html5Qrcode("reader");
+
+    function onScanSuccess(decodedText) {
+        document.getElementById("resultado").innerHTML =
+            "Código leído:<br><br>" + decodedText;
+    }
+
+    Html5Qrcode.getCameras().then(devices => {
+
+        if (devices && devices.length) {
+
+            const cameraId = devices[0].id;
+
+            qr.start(
+                cameraId,
+                {
+                    fps: 10,
+                    qrbox: 250
+                },
+                onScanSuccess
+            );
+
+        } else {
+            document.getElementById("resultado").innerHTML =
+                "No se detectó cámara";
+        }
+
+    }).catch(err => {
+        console.error(err);
+        document.getElementById("resultado").innerHTML =
+            "Error al acceder a la cámara";
+    });
+
+});
